a10c1259dbb76d1217a468e5431bb2b5fe6afda8
Kyle Ryan <rookoyole@gmail.com>
2021-03-16 22:39:43 -0700

    API calls returning data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
-
-
 function myFunction() {
-  var searchTerm = document.querySelector(`#searchVibe`).nodeValue;
-  // Create a variable to hold the value of rating
-  var rating= document.querySelector(`#rating`).nodeValue;
-  fetch('https://imdb.api.com/en/API/Keyword/k_wmu1kuc6' + searchTerm)
-  .then(function(response) {
-    //console.log("test");
-      return response.json();
-  })
-  . then(function(response) {
-      console.log(response);
-
-  });
-
-  fetch (
-    'https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=' + searchTerm + '&key=AIzaSyDSjKl3kb7QXntrm9amPQnhhqFQ2UhRwm0'
+  // Create a variable called `searchTerm` that will use `document.querySelector()` to target the `id` of the input
+  // Use `.value` to capture the value of the input and store it in the variable
+  var searchTerm = document.querySelector('#searchVibe').value;
+  console.log(searchTerm);
+  // Make a `fetch` request concatenating the `searchTerm` to the query URL
+  // Remember to add your API key at the end
+  //debugger
+  fetch(
+    'https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=' +
+    searchTerm +
+    '&key=AIzaSyDSjKl3kb7QXntrm9amPQnhhqFQ2UhRwm0'
   )
-  .then(function(response) {
-      return response. json();
-  })
-  . then(function(response) {
-      console.log(response);
+    .then(response => response.json())
+    .then(data => console.log(data));
 
-  });
+    fetch(
+      'https://imdb-api.com/en/API/Keyword/k_wmu1kuc6/' +
+    searchTerm
+    )
+      .then(response => response.json())
+      .then(data => console.log(data));
+
+
+    
+
 }
