21460a79873f1055500bfa102345d2afe878067c
Breedlove13 <b.breedlove@outlook.com>
2021-03-18 20:50:35 -0700

    music button working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,38 @@
+var responseContainerEL = document.getElementById("response");
 
+// function youtubeFunction()
+// preventDefault()
+//   // Create a variable called `searchTerm` that will use `document.querySelector()` to target the `id` of the input
+//   // Use `.value` to capture the value of the input and store it in the variable
+//   var searchTerm = document.querySelector("#searchVibe").value;
+//   // Make a `fetch` request concatenating the `searchTerm` to the query URL
+//   // Remember to add your API key at the end
+//   //debugger
+//   fetch(
+//     "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=happy&key=AIzaSyC6SlQNkAvha28BgZ7lnzE63fr3x-TIzzg"
+//   )
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then(function (res) {
+//       console.log(res);
+//       alert("iam all man");
+//       // Create a variable that will select the <div> where the GIF will be displayed
+//       var responseContainerEl = document.querySelector("#response-container");
 
-var responseContainerEL = document.getElementById('response')
+//       // Empty out the <div> before we append a GIF to it
+//       responseContainerEl.innerHTML = "";
 
+//       var gifImg = document.createElement("h1");
+//       gifImg.textContent = "Youtube Song : " + res.items[0].snippet.title;
 
-
-
-
-function youtubeFunction() {
-  // Create a variable called `searchTerm` that will use `document.querySelector()` to target the `id` of the input
-  // Use `.value` to capture the value of the input and store it in the variable
-  var searchTerm = document.querySelector('#searchVibe').value;
-  console.log(searchTerm);
-  // Make a `fetch` request concatenating the `searchTerm` to the query URL
-  // Remember to add your API key at the end
-  //debugger
-  fetch(
-    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=" +
-      searchTerm +
-      "&key=AIzaSyC6SlQNkAvha28BgZ7lnzE63fr3x-TIzzg"
-  )
-    .then((response) => response.json())
-    .then(function (response) {
-      console.log(response);
-      alert('iam all man')
-      // Create a variable that will select the <div> where the GIF will be displayed
-      var responseContainerEl = document.querySelector("#response-container");
-
-      // Empty out the <div> before we append a GIF to it
-      responseContainerEl.innerHTML = "";
-
-      var gifImg = document.createElement("h1");
-      gifImg.textContent = "Youtube Song : " + response.items[0].snippet.title;
-
-      // Append 'gifImg' to the <div>
-      responseContainerEl.appendChild(gifImg);
-    });
-
-}
-
-
+//       // Append 'gifImg' to the <div>
+//       responseContainerEl.appendChild(gifImg);
+//     });
+// }
 
 function imdbFunction() {
-  var searchTerm = document.getElementById('rating').value;
+  var searchTerm = document.getElementById("rating").value;
 
   //document.getElementById("demo").innerHTML = "You selected: " + x;
 
@@ -58,9 +49,37 @@ function imdbFunction() {
     });
 }
 
+document.querySelector(".button").addEventListener(
+  "click",
+  function (event) {
+    event;
 
+    var searchTerm = document.querySelector("#searchVibe").value;
+    // Make a `fetch` request concatenating the `searchTerm` to the query URL
+    // Remember to add your API key at the end
+    //debugge
+    event.preventDefault();
 
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=happy&key=AIzaSyC6SlQNkAvha28BgZ7lnzE63fr3x-TIzzg"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then(function (data) {
+        console.log(data);;
+        // Create a variable that will select the <div> where the GIF will be displayed
+        var responseContainerEl = document.querySelector("#response-container");
 
+        // Empty out the <div> before we append a GIF to it
+        responseContainerEl.innerHTML = "";
 
+        var gifImg = document.createElement("h1");
+        gifImg.textContent = "Youtube Song : " + data.items[0].snippet.title;
 
-
+        // Append 'gifImg' to the <div>
+        responseContainerEl.appendChild(gifImg);
+      });
+  },
+  false
+);
